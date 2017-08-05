54812d393bb7c023f858d9d4b326fe0ca167053c
jenny <xinning.ren@gmail.com>
2017-08-05 23:01:26 +1200

    add --file,--dry_run,--create_table to control script

--- user_upload.php
+++ user_upload.php
@@ -7,8 +7,52 @@ $username = "root";
 $password = "abc";
 $db = "csv";
 
-$fileName = "users";
-$tableName="users";
+$fileName = "";
+$tableName="AAusers";
+
+
+print_r($argv);
+
+function getCommandLines($argv){
+	
+	foreach($argv as $i => $str){
+		if ($i == 0) continue;
+		
+		
+		if(substr( $str, 0, 6 ) === "--file"){
+			global $fileName;
+
+			$start = strpos($str, '[');
+			$end = strpos($str, ']', $start + 1);
+			$length = $end - $start;
+			$result = substr($str, $start + 1, $length - 1);
+			
+			if(strpos($result, '.') !== false) {
+				die('This is invalid file. Please do not include any extension.');
+			}else{
+				$fileName = $result;
+			}
+	
+		}else if(substr( $str, 0, 16 ) === "--create_table"){				
+			getFile('create_table');
+				
+		}else if(substr( $str, 0, 9 ) === "--dry_run"){
+			if($fileName === ""){
+				echo "please set file name at beginning in command line";
+			}else{
+				getFile('dry_run');
+			}
+			
+				
+		}else if(substr( $str, 0, 6 ) === "help"){
+			echo"--file [csv file name] - this is the name of the CSV to be parsed \n".
+				"--create_table - this will cause the MySQL users table to be built \n";
+		}
+	   
+	}
+	
+	
+}
 
 //Connect to DB
 $conn = mysqli_connect($servername, $username, $password, $db);
@@ -132,14 +176,17 @@ function doCreateTable($handle){
 /**
  * Function is to open the csv file 
 */
-function getFile(){
+function getFile($action){
 	global $fileName;
 	
 	if(filesize($fileName.".csv") > 0){	
 		if (($handle = fopen($fileName.".csv", "r")) !== FALSE) {	
 			
-			//doDryRun($handle);
-			doCreateTable($handle);
+			if($action === 'create_table'){
+				doCreateTable($handle);
+			}else if($action === 'dry_run'){
+				doDryRun($handle);
+			}
 
 			fclose($handle);
 			
@@ -152,7 +199,7 @@ function getFile(){
 
 }
 
-getFile();
+getCommandLines($argv);
 
 
 ?>
