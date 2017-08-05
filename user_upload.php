9f8dbd06646331f3f44c5e8e03d786f22633f7f0
jenny <xinning.ren@gmail.com>
2017-08-05 21:26:46 +1200

    create table in database

--- user_upload.php
+++ user_upload.php
@@ -1,7 +1,22 @@
 <?php
-//error_reporting(E_ERROR);
+error_reporting(E_ERROR);
+
+//DB config
+$servername = "localhost";
+$username = "root";
+$password = "abc";
+$db = "csv";
 
 $fileName = "users";
+$tableName="ww";
+
+//Connect to DB
+$conn = mysqli_connect($servername, $username, $password, $db);
+if(!$conn){
+	die("Database connect error : ". mysqli_connect_errno() . PHP_EOL);
+}else{
+	echo "success to connect to MYSQL! \n";
+}
 
 //
 /**
@@ -10,6 +25,8 @@ $fileName = "users";
 */
 function doDryRun($handle){
 	$row = 0;
+	
+	echo "\nData from csv file: \n";
 	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {	
 		$num = count($data);
 		$row++;	
@@ -26,10 +43,49 @@ function doDryRun($handle){
 
 /**
  * Function is to create the DB table 
- * 
+ * @param $handle -- the opened csv file 
 */
-function doCreateTable(){
-
+function doCreateTable($handle){
+	global $tableName;
+	global $fileName;
+	global $conn;
+	
+	$fields = "";
+	$fieldsInsert = "";
+	
+	//Get the first row of data as sql
+	if(($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+		$num = count($data);
+		$fieldsInsert .= '(';
+		for ($c=0; $c < $num; $c++) {
+			$trim_data= trim($data[$c]);
+			$fieldsInsert .=($c==0) ? '' : ', ';
+			$fieldsInsert .="`".$trim_data."`"; 
+			$fields .="`".$trim_data."` varchar(1000) DEFAULT NULL,";					
+		}
+	$fieldsInsert .= ')';
+	}
+	
+	//Drop table if it exists
+	$query = "SELECT NAME FROM '".$tableName."'";
+	$result = mysqli_query($conn, $query);			
+	if(empty($result)) {
+		mysqli_query($conn,'DROP TABLE IF EXISTS `'.$tableName.'`') or die(mysql_error());
+	}
+	
+	//create table
+	$sql = "CREATE TABLE `".$tableName."` (
+        `".$tableName."Id` int(100) unsigned NOT NULL AUTO_INCREMENT,
+        ".$fields."
+        PRIMARY KEY (`".$tableName."Id`)
+    ) ";
+	
+	$retval = mysqli_query($conn,$sql);
+	
+	//insert data to table
+	if(! $retval ){
+		die('Could not create table: ' . mysql_error());
+	}
 }
 
 
@@ -42,7 +98,8 @@ function getFile(){
 	if(filesize($fileName.".csv") > 0){	
 		if (($handle = fopen($fileName.".csv", "r")) !== FALSE) {	
 			
-			doDryRun($handle);
+			//doDryRun($handle);
+			doCreateTable($handle);
 
 			fclose($handle);
 			
@@ -56,4 +113,6 @@ function getFile(){
 }
 
 getFile();
+
+
 ?>
